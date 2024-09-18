f595df8acdb1f6df9c52bb615f5472143423040c
TheBoiLeroy <ialsanto@ucsc.edu>
2024-09-18 13:43:33 -0700

    navbar

--- cap_website/src/App.js
+++ cap_website/src/App.js
@@ -1,17 +1,14 @@
 import React from 'react';
-// import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Home from './components/Home';
 import About from './components/About';
 import Contact from './components/Contact';
-import Header from './components/Header';
-// import Footer from './components/Footer';
-// import EstimateRequest from './components/EstimateRequest';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Header />
+      <Navbar />
       <Home />
       <About />
       <Contact />
---
+++ cap_website/src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './Navbar.css'; // We'll style the navbar in this file
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo-container">
+        <img 
+          src="https://via.placeholder.com/100" 
+          alt="Logo Placeholder" 
+          className="logo"
+        />
+      </div>
+      <ul className="nav-links">
+        <li><a href="#home">Home</a></li>
+        <li><a href="#about">About</a></li>
+        <li><a href="#contact">Contact</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
